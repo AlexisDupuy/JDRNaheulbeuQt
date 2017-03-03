7adc1752d92d298438e13abb5f27224a51f25728
aquakiss <romain.menard17@gmail.com>
2017-03-03 11:14:02 +0100

    Wholo moi sa marche

--- JDRQT/modeljson.cpp
+++ JDRQT/modeljson.cpp
@@ -25,90 +25,75 @@ void ModelJSON::setSNom(const QString &sNom)
     m_sNom = sNom;
 }
 
-
 double ModelJSON::sSexe() const
 {
     return m_sSexe;
 }
-
 
 void ModelJSON::setSSexe(double sSexe)
 {
     m_sSexe = sSexe;
 }
 
-
 double ModelJSON::sOrigin() const
 {
     return m_sOrigin;
 }
-
 
 void ModelJSON::setSOrigin(double sOrigin)
 {
     m_sOrigin = sOrigin;
 }
 
-
 double ModelJSON::sMetier() const
 {
     return m_sMetier;
 }
-
 
 void ModelJSON::setSMetier(double sMetier)
 {
     m_sMetier = sMetier;
 }
 
-
 double ModelJSON::iEv() const
 {
     return m_iEv;
 }
-
 
 void ModelJSON::setIEv(double iEv)
 {
     m_iEv = iEv;
 }
 
-
 double ModelJSON::iEa() const
 {
     return m_iEa;
 }
-
 
 void ModelJSON::setIEa(double iEa)
 {
     m_iEa = iEa;
 }
 
-
 double ModelJSON::iMagPhy() const
 {
     return m_iMagPhy;
 }
-
 
 void ModelJSON::setIMagPhy(double iMagPhy)
 {
     m_iMagPhy = iMagPhy;
 }
 
-
 double ModelJSON::iMagPsy() const
 {
     return m_iMagPsy;
 }
 
-
 void ModelJSON::setIMagPsy(double iMagPsy)
 {
     m_iMagPsy = iMagPsy;
 }
-
 
 double ModelJSON::iResMag() const
 {
@@ -120,12 +105,10 @@ void ModelJSON::setIResMag(double iResMag)
     m_iResMag = iResMag;
 }
 
-
 double ModelJSON::iCourage() const
 {
     return m_iCourage;
 }
-
 
 void ModelJSON::setICourage(double iCourage)
 {
@@ -134,22 +117,18 @@ void ModelJSON::setICourage(double iCourage)
 
 double ModelJSON::iIntel() const
 {
-
-    return m_idoubleel;
+    return m_iIntel;
 }
 
-
-void ModelJSON::setIntel(double idoubleel)
+void ModelJSON::setIntel(double Intel)
 {
-
-    m_idoubleel = idoubleel;
+    m_iIntel = Intel;
 }
 
 double ModelJSON::iChar() const
 {
     return m_iChar;
 }
-
 
 void ModelJSON::setIChar(double iChar)
 {
@@ -161,126 +140,105 @@ double ModelJSON::iAddr() const
     return m_iAddr;
 }
 
-
 void ModelJSON::setIAddr(double iAddr)
 {
     m_iAddr = iAddr;
 }
-
 
 double ModelJSON::iForce() const
 {
     return m_iForce;
 }
 
-
 void ModelJSON::setIForce(double iForce)
 {
     m_iForce = iForce;
 }
-
 
 double ModelJSON::iAttaq() const
 {
     return m_iAttaq;
 }
 
-
 void ModelJSON::setIAttaq(double iAttaq)
 {
     m_iAttaq = iAttaq;
 }
-
 
 double ModelJSON::iParad() const
 {
     return m_iParad;
 }
 
-
 void ModelJSON::setIParad(double iParad)
 {
     m_iParad = iParad;
 }
-
 
 double ModelJSON::iNiveau() const
 {
     return m_iNiveau;
 }
 
-
 void ModelJSON::setINiveau(double iNiveau)
 {
     m_iNiveau = iNiveau;
 }
-
 
 double ModelJSON::iExpe() const
 {
     return m_iExpe;
 }
 
-
 void ModelJSON::setIExpe(double iExpe)
 {
     m_iExpe = iExpe;
 }
-
 
 double ModelJSON::iDestin() const
 {
     return m_iDestin;
 }
 
-
 void ModelJSON::setIDestin(double iDestin)
 {
     m_iDestin = iDestin;
 }
-
 
 double ModelJSON::iOr() const
 {
     return m_iOr;
 }
 
-
 void ModelJSON::setIOr(double iOr)
 {
     m_iOr = iOr;
 }
-
 
 double ModelJSON::iArgent() const
 {
     return m_iArgent;
 }
 
-
 void ModelJSON::setIArgent(double iArgent)
 {
     m_iArgent = iArgent;
 }
-
 
 double ModelJSON::iCuivre() const
 {
     return m_iCuivre;
 }
 
-
 void ModelJSON::setICuivre(double iCuivre)
 {
     m_iCuivre = iCuivre;
 }
 
-
 double ModelJSON::iPr() const
 {
     return m_iPr;
 }
-
 
 void ModelJSON::setIPr(double iPr)
 {
