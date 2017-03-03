3c2517ba8a6aa0ff27f3d5e9aa0eb6aa93b5bcf0
aquakiss <romain.menard17@gmail.com>
2017-03-03 10:12:06 +0100

    Add var in ModelJSON and Getter / Setter

--- JDRQT/modeljson.cpp
+++ JDRQT/modeljson.cpp
@@ -1,6 +1,237 @@
 #include "modeljson.h"
+#include "QMap"
 
 ModelJSON::ModelJSON(QObject *parent) : QObject(parent)
 {
 
+}
+
+QString ModelJSON::sNom() const
+{
+    return m_sNom;
+}
+
+void ModelJSON::setSNom(const QString &sNom)
+{
+    m_sNom = sNom;
+}
+
+int ModelJSON::sSexe() const
+{
+    return m_sSexe;
+}
+
+void ModelJSON::setSSexe(int sSexe)
+{
+    m_sSexe = sSexe;
+}
+
+int ModelJSON::sOrigin() const
+{
+    return m_sOrigin;
+}
+
+void ModelJSON::setSOrigin(int sOrigin)
+{
+    m_sOrigin = sOrigin;
+}
+
+int ModelJSON::sMetier() const
+{
+    return m_sMetier;
+}
+
+void ModelJSON::setSMetier(int sMetier)
+{
+    m_sMetier = sMetier;
+}
+
+int ModelJSON::iEv() const
+{
+    return m_iEv;
+}
+
+void ModelJSON::setIEv(int iEv)
+{
+    m_iEv = iEv;
+}
+
+int ModelJSON::iEa() const
+{
+    return m_iEa;
+}
+
+void ModelJSON::setIEa(int iEa)
+{
+    m_iEa = iEa;
+}
+
+int ModelJSON::iMagPhy() const
+{
+    return m_iMagPhy;
+}
+
+void ModelJSON::setIMagPhy(int iMagPhy)
+{
+    m_iMagPhy = iMagPhy;
+}
+
+int ModelJSON::iMagPsy() const
+{
+    return m_iMagPsy;
+}
+
+void ModelJSON::setIMagPsy(int iMagPsy)
+{
+    m_iMagPsy = iMagPsy;
+}
+
+int ModelJSON::iResMag() const
+{
+    return m_iResMag;
+}
+
+void ModelJSON::setIResMag(int iResMag)
+{
+    m_iResMag = iResMag;
+}
+
+int ModelJSON::iCourage() const
+{
+    return m_iCourage;
+}
+
+void ModelJSON::setICourage(int iCourage)
+{
+    m_iCourage = iCourage;
+}
+
+int ModelJSON::iIntel() const
+{
+    return m_iIntel;
+}
+
+void ModelJSON::setIIntel(int iIntel)
+{
+    m_iIntel = iIntel;
+}
+
+int ModelJSON::iChar() const
+{
+    return m_iChar;
+}
+
+void ModelJSON::setIChar(int iChar)
+{
+    m_iChar = iChar;
+}
+
+int ModelJSON::iAddr() const
+{
+    return m_iAddr;
+}
+
+void ModelJSON::setIAddr(int iAddr)
+{
+    m_iAddr = iAddr;
+}
+
+int ModelJSON::iForce() const
+{
+    return m_iForce;
+}
+
+void ModelJSON::setIForce(int iForce)
+{
+    m_iForce = iForce;
+}
+
+int ModelJSON::iAttaq() const
+{
+    return m_iAttaq;
+}
+
+void ModelJSON::setIAttaq(int iAttaq)
+{
+    m_iAttaq = iAttaq;
+}
+
+int ModelJSON::iParad() const
+{
+    return m_iParad;
+}
+
+void ModelJSON::setIParad(int iParad)
+{
+    m_iParad = iParad;
+}
+
+int ModelJSON::iNiveau() const
+{
+    return m_iNiveau;
+}
+
+void ModelJSON::setINiveau(int iNiveau)
+{
+    m_iNiveau = iNiveau;
+}
+
+int ModelJSON::iExpe() const
+{
+    return m_iExpe;
+}
+
+void ModelJSON::setIExpe(int iExpe)
+{
+    m_iExpe = iExpe;
+}
+
+int ModelJSON::iDestin() const
+{
+    return m_iDestin;
+}
+
+void ModelJSON::setIDestin(int iDestin)
+{
+    m_iDestin = iDestin;
+}
+
+int ModelJSON::iOr() const
+{
+    return m_iOr;
+}
+
+void ModelJSON::setIOr(int iOr)
+{
+    m_iOr = iOr;
+}
+
+int ModelJSON::iArgent() const
+{
+    return m_iArgent;
+}
+
+void ModelJSON::setIArgent(int iArgent)
+{
+    m_iArgent = iArgent;
+}
+
+int ModelJSON::iCuivre() const
+{
+    return m_iCuivre;
+}
+
+void ModelJSON::setICuivre(int iCuivre)
+{
+    m_iCuivre = iCuivre;
+}
+
+int ModelJSON::iPr() const
+{
+    return m_iPr;
+}
+
+void ModelJSON::setIPr(int iPr)
+{
+    m_iPr = iPr;
 }
--- JDRQT/modeljson.h
+++ JDRQT/modeljson.h
@@ -3,11 +3,134 @@
 
 #include <QObject>
 
+class QMap;
+
 class ModelJSON : public QObject
 {
     Q_OBJECT
 public:
     explicit ModelJSON(QObject *parent = 0);
+
+    QString sNom() const;
+
+    void setSNom(const QString &sNom);
+
+    int sSexe() const;
+
+    void setSSexe(int sSexe);
+
+    int sOrigin() const;
+
+    void setSOrigin(int sOrigin);
+
+    int sMetier() const;
+
+    void setSMetier(int sMetier);
+
+    int iEv() const;
+
+    void setIEv(int iEv);
+
+    int iEa() const;
+
+    void setIEa(int iEa);
+
+    int iMagPhy() const;
+
+    void setIMagPhy(int iMagPhy);
+
+    int iMagPsy() const;
+
+    void setIMagPsy(int iMagPsy);
+
+    int iResMag() const;
+
+    void setIResMag(int iResMag);
+
+    int iCourage() const;
+
+    void setICourage(int iCourage);
+
+    int iIntel() const;
+
+    void setIIntel(int iIntel);
+
+    int iChar() const;
+
+    void setIChar(int iChar);
+
+    int iAddr() const;
+
+    void setIAddr(int iAddr);
+
+    int iForce() const;
+
+    void setIForce(int iForce);
+
+    int iAttaq() const;
+
+    void setIAttaq(int iAttaq);
+
+    int iParad() const;
+
+    void setIParad(int iParad);
+
+    int iNiveau() const;
+
+    void setINiveau(int iNiveau);
+
+    int iExpe() const;
+
+    void setIExpe(int iExpe);
+
+    int iDestin() const;
+
+    void setIDestin(int iDestin);
+
+    int iOr() const;
+
+    void setIOr(int iOr);
+
+    int iArgent() const;
+
+    void setIArgent(int iArgent);
+
+    int iCuivre() const;
+
+    void setICuivre(int iCuivre);
+
+    int iPr() const;
+
+    void setIPr(int iPr);
+
+private:
+    // Variable de la fiche :
+    // aFiche : array[Fiche]
+    QString m_sNom; // : string
+    int m_sSexe; // : string
+    int m_sOrigin; // : enum(string)
+    int m_sMetier; // : enum(string)
+    int m_iEv; // : int // PV
+    int m_iEa; // : int // Mana
+    int m_iMagPhy; // : int
+    int m_iMagPsy; // : int
+    int m_iResMag; // : int
+    int m_iCourage; // : int
+    int m_iIntel; // : int
+    int m_iChar; // : int
+    int m_iAddr; // : int
+    int m_iForce; // : int
+    int m_iAttaq; // : int
+    int m_iParad; // : int
+    int m_iNiveau; // : int
+    int m_iExpe; // : int
+    int m_iDestin; // : int
+    int m_iOr; // : int
+    int m_iArgent; // : int
+    int m_iCuivre; // : int
+    int m_iPr; // : int // protection totale
+    QMap<QString, QString> m_cArmure; // : collection(string) // nom = nom armure (string), valeur = pr de l'armure (int), rupt = rupture de l'armure (string))
+    QMap<QString, QString> m_cArme; // : collection(string) // nom = nom arme (string), valeur = pi de l'arme (string), rupt = rupture de l'arme (string))
 
 signals:
 
